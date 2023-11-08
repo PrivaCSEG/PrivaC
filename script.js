3e7aa2014d2ad5171d17e66ec64b98c35506e7c0
Lining Ao <Nicolea25418@gmail.com>
2023-11-07 19:07:03 -0500

    fixed javascript and edited some of the css

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-document.body.onmousemove = function(e) {
-    const mask = document.querySelector('.mask');
-    const x = e.clientX;
-    const y = e.clientY;
-    
-    if (
-        x >= mask.offsetLeft && x <= mask.offsetLeft + mask.offsetWidth &&
-        y >= mask.offsetTop && y <= mask.offsetTop + mask.offsetHeight
-    ) {
-        // Mouse is inside the .mask element
-        mask.style.setProperty('--x', x + 'px');
-        mask.style.setProperty('--y', y + 'px');
-    } else {
-        // Mouse is outside the .mask element
-        mask.style.setProperty('--x', '-10000px'); // Move circle out of bounds
-        mask.style.setProperty('--y', '-10000px'); // Move circle out of bounds
-    }
+const mask = document.querySelector('.mask');
+
+document.addEventListener('mousemove', (e) => {
+  // Retrives coordinates of mouse (x and y)
+  const x = e.clientX;
+  const y = e.clientY;
+
+  // Get the position and dimensions of the "mask" div
+  const maskRect = mask.getBoundingClientRect();
+
+  // Check if the mouse is within the boundaries of the "mask" div
+  if (
+    x >= maskRect.left &&
+    x <= maskRect.right &&
+    y >= maskRect.top &&
+    y <= maskRect.bottom
+  ) {
+    // Actions if mouse is inside the "mask" div
+    mask.style.setProperty('--x', x + 'px');
+    mask.style.setProperty('--y', y + 'px');
+  } else {
+    // Actions is mouse is outside the "mask" div
+    mask.style.setProperty('--x', '-10000px'); // Move element out of bounds
+    mask.style.setProperty('--y', '-10000px'); // Move element out of bounds
   }
+});
 
 /* FAQ show and hide */
 $(document).ready(function() {
@@ -26,3 +34,4 @@ $(document).ready(function() {
       $(this).find('span').text('-');
     });
   });
+
