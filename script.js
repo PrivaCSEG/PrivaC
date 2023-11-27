28f06574cbfa33ce8a471abc6f79284e59c27fac
Lining Ao <Nicolea25418@gmail.com>
2023-11-26 20:17:22 -0500

    final changes

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-const mask = document.querySelector('.mask');
-
-document.addEventListener('mousemove', (e) => {
-  // Retrives coordinates of mouse (x and y)
-  const x = e.clientX;
-  const y = e.clientY;
-
-  // Get the position and dimensions of the "mask" div
-  const maskRect = mask.getBoundingClientRect();
-
-  // Check if the mouse is within the boundaries of the "mask" div
-  if (
-    x >= maskRect.left &&
-    x <= maskRect.right &&
-    y >= maskRect.top &&
-    y <= maskRect.bottom
-  ) {
-    // Actions if mouse is inside the "mask" div
-    mask.style.setProperty('--x', x + 'px');
-    mask.style.setProperty('--y', y + 'px');
-  } else {
-    // Actions is mouse is outside the "mask" div
-    mask.style.setProperty('--x', '-10000px'); // Move element out of bounds
-    mask.style.setProperty('--y', '-10000px'); // Move element out of bounds
-  }
-});
 
 /* FAQ show and hide */
 $(document).ready(function() {
